5424080842926d055332e4c9af6ee2bde945a37e
FredKKLind <fredrik.lindholm@outlook.com>
2024-02-04 14:01:45 +0200

    removed Angular hydration request

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,4 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
+import { mergeApplicationConfig} from '@angular/core';
 import { appConfig } from './app.config';
-import { provideClientHydration } from '@angular/platform-browser';
 
-const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideClientHydration()
-  ]
-};
-
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const config = mergeApplicationConfig(appConfig);
