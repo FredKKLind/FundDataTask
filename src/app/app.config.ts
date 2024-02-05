d1f9f4d5c563bcee12fe1bcd91a5ae723bb4e260
FredKKLind <fredrik.lindholm@outlook.com>
2024-02-05 18:40:17 +0200

    changed README

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,5 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [provideRouter(routes), provideAnimationsAsync('noop'), provideAnimationsAsync('noop')]
 };
